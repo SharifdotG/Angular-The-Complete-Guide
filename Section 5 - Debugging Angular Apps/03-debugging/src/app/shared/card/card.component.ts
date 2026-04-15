97047a5f8c3b4fcafb117770c331a5b4bf273b70
Sharif Md. Yousuf <sharifmdyousuf007@gmail.com>
2026-04-15 17:49:24 +0600

    refactor: standardize quotation marks and improve formatting across multiple files

--- Section 5 - Debugging Angular Apps/03-debugging/src/app/shared/card/card.component.ts
+++ Section 5 - Debugging Angular Apps/03-debugging/src/app/shared/card/card.component.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-card',
+  selector: "app-card",
   standalone: true,
   imports: [],
-  templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  templateUrl: "./card.component.html",
+  styleUrl: "./card.component.css",
 })
-export class CardComponent {
-
-}
+export class CardComponent {}
